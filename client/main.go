0c01451125f12f1af575ddb855c8eea4656547d0
Fatih YILMAZ <fatih22yilmaz@gmail.com>
2022-06-26 16:02:39 +0300

    Adds go breaker implementations

--- client/main.go
+++ client/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"github.com/sony/gobreaker"
 	"gobreaker-experiment/infrastructure/http"
 	"log"
 	netHttp "net/http"
+	"time"
 )
 
 var downstreamErrCount int
@@ -18,32 +19,59 @@ type clientStruct struct {
 	circuitBreaker *gobreaker.CircuitBreaker
 }
 
-func (c *clientStruct) main() {
+func newClientStruct(circuitBreaker *gobreaker.CircuitBreaker) *clientStruct {
+	return &clientStruct{circuitBreaker: circuitBreaker}
+}
+
+func main() {
 	downstreamErrCount = 0
 	circuitOpenErrCount = 0
 	timeoutErrCount = 0
 
-	c.circuitBreaker = gobreaker.NewCircuitBreaker(gobreaker.Settings{
+	circuitBreaker := gobreaker.NewCircuitBreaker(gobreaker.Settings{
 		Name:        "GoBreaker",
 		MaxRequests: 3,
 		Interval:    0,
-		Timeout:     5,
+		Timeout:     1 * time.Second,
 		OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
 			log.Printf("Gobreaker's state changed from: %v, to: %v", from, to)
 		},
+		IsSuccessful: func(err error) bool {
+			if err != nil {
+				log.Printf("err: %v", err.Error())
+			}
+			return false
+		},
 	})
 
-	netHttp.HandleFunc("/", handlerFunc)
+	c := newClientStruct(circuitBreaker)
+
+	netHttp.HandleFunc("/", c.handlerFunc)
 
 	log.Fatal(netHttp.ListenAndServe(":8080", nil))
-
 }
 
 func (c *clientStruct) handlerFunc(w netHttp.ResponseWriter, r *netHttp.Request) {
-	var response []byte
-	c.circuitBreaker.Execute(func() (interface{}, error) {
+	response, err := c.circuitBreaker.Execute(func() (interface{}, error) {
+		responseByteArray, err := httpClient.Get("http://localhost:8081", "/server-path")
+		if err != nil {
+			return nil, err
+		}
 
+		return responseByteArray, nil
 	})
-	fmt.Printf("\ndownstreamErrCount=%d, circuitOpenErrCount=%d, timeoutErrCount=%d", downstreamErrCount, circuitOpenErrCount, timeoutErrCount)
 
+	if err == nil {
+		w.Write(response.([]byte))
+	}
+
+	// ErrOpenState can be handled as fallback method.
+	if err != nil && errors.Is(err, gobreaker.ErrOpenState) {
+		log.Printf("error open state: %v", err.Error())
+	}
+
+	// ErrTooManyRequests can be handled when too many requests
+	if err != nil && errors.Is(err, gobreaker.ErrTooManyRequests) {
+		log.Printf("error too many requests: %v", err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module gobreaker-experiment
 
 go 1.17
 
-require github.com/sony/gobreaker v0.5.0 // indirect
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/sony/gobreaker v0.5.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.37.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
